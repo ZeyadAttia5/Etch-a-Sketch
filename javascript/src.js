a8cdb1818ef3ce63b2d69339792f8dbe1d8a3cff
Zeyad_Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-01-25 00:22:11 +0200

    a square in the grid turns black if hovered

--- javascript/src.js
+++ javascript/src.js
@@ -10,3 +10,10 @@ for(let i = 0; i <16; i++){
         container.appendChild(div);
     }
 }
+
+function modify(e){
+    e.target.classList.add('modified-box');
+}
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => box.addEventListener('mouseover', modify));
