731fe5a5dffe8e992c022ea243e55a7ffcaf311c
Zeyad_Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-01-25 10:22:52 +0200

    Added draw, earse and resize functionality
    Phew!! Huge update!
    -Added draw feature by click and drag mechanism
    -Added erase feature by hovering the mouse over the black pixels
    -Added resize feature that can go up to 100x100 in the fixed-size-grid
    
    The default grid size has been changed to 25x25!!

--- javascript/src.js
+++ javascript/src.js
@@ -3,17 +3,78 @@
 
 const container = document.querySelector('#container')
 
-for(let i = 0; i <16; i++){
-    for (let j = 0; j < 16; j++) {
+for(let i = 0; i <25; i++){
+    for (let j = 0; j < 25; j++) {
         const div = document.createElement('div');
         div.classList.toggle('box');
         container.appendChild(div);
     }
 }
 
-function modify(e){
-    e.target.classList.add('modified-box');
+
+let boxes = document.querySelectorAll('.box');
+
+const erase = document.querySelector('#erase');
+erase.addEventListener('click', clear);
+
+const pen = document.querySelector('#pen');
+pen.addEventListener('click', modify);
+
+const size = document.querySelector('#size');
+size.addEventListener('click', changeSize);
+
+
+function changeSize(e){
+    let s = prompt()
+    s = parseInt(s);
+    if(s > 0 && s <= 100){
+        while(container.hasChildNodes()){
+            container.removeChild(container.firstChild);
+        }
+        for(let i = 0; i <s; i++){
+            for (let j = 0; j < s; j++) {
+                const div = document.createElement('div');
+                div.classList.add('box');
+                container.appendChild(div);
+                div.style.height = `${s*(625/(s*s))}px`;
+                div.style.width = `${s*(625/(s*s))}px`;
+            }
+        }
+        //container.style.height = `${s*s}px`;
+        //container.style.width = `${s*s}px`;
+        container.style.gridTemplateColumns = `repeat(${s}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${s}, 1fr)`;
+        console.log(boxes.length);
+        boxes = document.querySelectorAll('.box');
+    }
+    else
+        return
 }
 
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => box.addEventListener('mouseover', modify));
+
+function clear(e){
+    boxes.forEach(box => box.addEventListener('mousemove', clean));
+}
+function clean(e){
+    e.target.classList.remove('modified-box');
+}
+
+function modify(e){
+    
+    boxes.forEach(box => box.removeEventListener('mousemove', clean))
+    boxes.forEach(box => box.addEventListener('mousedown', mouseDownHandle));
+    boxes.forEach(box => box.addEventListener('mouseup', removePen));
+}
+
+function removePen(e){
+    boxes.forEach(box => box.removeEventListener('mousemove', mouseMoveHandle));
+}
+
+function mouseDownHandle(e){
+    boxes.forEach(box => box.addEventListener('mousemove', mouseMoveHandle));
+}
+
+function mouseMoveHandle(e){
+    e.target.classList.add('modified-box');
+    
+}
